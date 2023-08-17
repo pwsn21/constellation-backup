dc87e4bee0938fb976273a4892df51c465ffad72
pwsn21 <pwsn21@gmail.com>
2023-08-17 14:27:23 -0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@
                 
             </div>
             <div>
-            <button class="mobile-menu-button p-4 focus:bg-gray-600">
+            <button class="mobile-menu-button p-4 focus:bg-gray-600" >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>
             </button>
             </div>
