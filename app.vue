b4a6beceaf1eab1c376f37ac40c15de11bc08fae
DosAge <mynamewasthieved@gmail.com>
2023-08-23 08:57:49 -0700

    Added Authentication through Firebase Auth with a simple demo form and a quick page for status and testing

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NuxtPage />
+    </div>
+</template>
+<script setup>
+const firebaseUser = useFirebaseUser();
+const authNeeded = useAuthNeeded();
+
+onMounted(async () => {
+    await initUser()
+})
+</script>
