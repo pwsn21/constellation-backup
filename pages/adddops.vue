4cdffe1ad33739358413dc41d8974b6dd3071ace
pwsn21 <pwsn21@gmail.com>
2023-09-01 09:52:28 -0700

    where to put the return function!!?

--- pages/adddops.vue
+++ pages/adddops.vue
@@ -71,8 +71,9 @@
                 </ul>
                 <button                                 
                             class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-                            Add DOPS
+                            Add DOPS 
                             </button>
+                            {{ firebaseUser.uid }}
                 </form>
     
 </div>
@@ -86,6 +87,13 @@
 
 import {collection, addDoc, getFirestore} from "firebase/firestore"; 
 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const firebaseUser = useFirebaseUser();
+const auth = getAuth();
+
+
+
 const db = getFirestore();
 
 const shiftdate = ref ('')
@@ -96,6 +104,19 @@ const dopsskill = ref ('')
 
 
 
+const userid = onAuthStateChanged(auth, (firebaseUser) => {
+   if (firebaseUser) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = firebaseUser.uid;
+    console.log (uid)
+    
+  } else {
+
+  }
+})
+
+console.log(userid)
 
 const dopscollection = collection(db, 'users', 'testid', 'dops')
 
